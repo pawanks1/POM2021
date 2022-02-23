7f623b32587738e719b54c7f10a17e69589c541f
Dev Karan <Dev.karan@algoworks.com>
2022-02-24 03:21:38 +0530

    added modified driverfactory file

--- src/main/java/com/qa/opencart/Driverfactory/DriverFactory.java
+++ src/main/java/com/qa/opencart/Driverfactory/DriverFactory.java
@@ -1,13 +1,9 @@
 package com.qa.opencart.Driverfactory;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class DriverFactory {
-
+public class DriverFactory{
 	public WebDriver driver;
 	
 	/**
@@ -16,37 +12,28 @@ public class DriverFactory {
 	 * @param browserName
 	 * @return this will return the driver
 	 */
+	
 	public WebDriver init_driver(String browserName) {
 		System.out.println("this is browser name " + browserName);
-		
-		if (browserName.equals("chrome")) {
+		if (browserName.equals("chrome")) {	
 			WebDriverManager.chromedriver().setup();
              driver = new ChromeDriver();
-		
-			}
+			}	
 		else if (browserName.equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
-
-		else if (browserName.equals("safari")) {
-			
-			
-			
+		else if (browserName.equals("safari")) {	
 			System.out.println("please pass the right browser: " + browserName);
 		}
-
 		driver.manage().window().fullscreen();
 		driver.manage().deleteAllCookies();
 		driver.get("https://demo.opencart.com/index.php?route=account/login");
-
 		return driver;
-	}
-
-	
-	
-	
-	
-	
-	
+	}		
 }
+
+
+
+
+
