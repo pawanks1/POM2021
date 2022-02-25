d3d5df34acb983e53de8d3ad03d23d01ab305673
Dev Karan <Dev.karan@algoworks.com>
2022-02-26 02:27:24 +0530

    added ordercopy page

---
+++ src/test/java/com/qa/opencart/tests/COPYBRANCH.java
@@ -0,0 +1,45 @@
+package com.qa.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qa.openkart.utils.constant;
+
+public class COPYBRANCH extends BaseTest{
+
+	
+
+	@Test(priority=1)
+	public void LoginPageTitle() {
+		
+		String actualTitle =loginpage.getLoginPageTitle();
+		
+		System.out.println("my actual title "+ actualTitle);
+		
+Assert.assertTrue(actualTitle.contains(constant.LOGIN_PAGE_TITLE));		
+		
+	}
+	
+	@Test(priority=2)
+	public void loginurl() {
+		
+		String actUrl=loginpage.getLoginUrl();
+		
+		Assert.assertTrue(actUrl.contains(constant.LOGIN_PAGE_URL));
+		
+		
+	}
+	@Test(priority=3)
+	public void forgotPasswordLink() {
+		 boolean forgotpwd = loginpage.isForgotPasswordLinkExits();
+		Assert.assertTrue(forgotpwd);
+	}
+	@Test(priority=4)
+	public void loginInapplication() {
+		
+		
+	loginpage.doLogin("pawankumar@gmail.com", "123pawan");	
+	
+	
+	}
+}
